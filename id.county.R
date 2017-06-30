05e0ac7bbb2afe7f25008a3e82bec6215b64315b
Stephen Wood <stephen.wood@CT201-SWOOD.wireless.yale.internal>
2017-06-30 11:20:49 -0400

    Finished geocoding by zip, address, or latlong

--- id.county.R
+++ id.county.R
@@ -1,17 +1,34 @@
+# read in multiple sources of georeference data and return lat/long coordinates
+
 id.county <- function(value,type){
   if(type == 'zip'){
-    require(noncensus)
-    data(zip_codes)
-    data(counties)
+    # using ggmap interface with google API
+    require(ggmap)
+    geo_reply = geocode(value, output='all', messaging=TRUE, override_limit=TRUE)
+    #     county <- geo_reply$results[[1]]$address_components[[5]]$long_name
+    return(c(geo_reply$results[[1]]$geometry$location$lat, geo_reply$results[[1]]$geometry$location$lng))
     
-    counties$fips <- interaction(counties$state_fips, counties$county_fips)    
-    zip_codes$fips =  as.numeric(as.character(zip_codes$fips))
-    
-    temp = subset(zip_codes, as.numeric(zip) == value)    
-    subset(counties, fips == temp$fips)
+    # # using zipcode centerpoints
+    # # requires downloading dataset
+    # require(readr)
+    # temp <- tempfile()
+    # download.file("http://federalgovernmentzipcodes.us/free-zipcode-database-Primary.csv",temp)
+    # data <- read_csv(temp)
+    # unlink(temp)
+    # return(as.numeric(data[which(data$Zipcode==value),c('Lat','Long')]))
+  }
+  else if(type == 'address'){
+    # inspiration for this approach from: http://www.shanelynn.ie/massive-geocoding-with-r-and-google-maps/
+    # could also do directly from census: https://geocoding.geo.census.gov/geocoder/Geocoding_Services_API.html#_Toc379292356
+    require(ggmap)
+    geo_reply = geocode(value, output='all', messaging=TRUE, override_limit=TRUE)
+    #     county <- geo_reply$results[[1]]$address_components[[5]]$long_name
+    return(c(geo_reply$results[[1]]$geometry$location$lat, geo_reply$results[[1]]$geometry$location$lng))
   }
   else if(type == 'latlong'){
-    require(httr)
-    return(content(GET(sprintf("http://data.fcc.gov/api/block/find?format=json&latitude=%f&longitude=%f&showall=true",value[1],value[2])))$County)
+    return(value)
+    # could use reverse geocoding with google API to get county information from latlong
+    # require(httr)
+    # return(content(GET(sprintf("http://data.fcc.gov/api/block/find?format=json&latitude=%f&longitude=%f&showall=true",value[1],value[2])))$County)
   }
 }
