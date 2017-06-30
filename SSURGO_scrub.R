f1d4f40321b6c60d67828bf7e85264be78aa5a7c
Stephen Wood <stephen.wood@CT201-SWOOD.wireless.yale.internal>
2017-06-30 11:23:43 -0400

    Beta script to scrub RaCA from latlong

--- SSURGO_scrub.R
+++ SSURGO_scrub.R
@@ -7,27 +7,28 @@
 #install.packages("sp")       # For defining points
 
 # load packages
-library(soilDB)
-library(sp)
-library(rgdal)
-library(rgeos)
-library(httr)
-library(jsonlite)
 
-library(sp)
-library(maps)
-library(maptools)
+som.score <- function(loc,type.enter,som=1){
+  require(soilDB)         # For querying SSURGO and RaCA
+  require(sp)             # For defining spatial points
 
-
-# create mock farm location
-p <- SpatialPoints(cbind(-122.315833333,37.128055556), proj4string = CRS('+proj=longlat +datum=WGS84'))
-
-# get mukey info for point
-mu.info <- SDA_make_spatial_query(p)
-
-# query SDA for mukey
-query <- paste("SELECT *
-               FROM component
-               WHERE mukey IN (",paste(unique(mu.info$mukey),collapse=","),")")
-SDA.sub <- SDA_query(query)
-
+  # determine farm location
+  p <- SpatialPoints(cbind(id.county(value=loc,type=type.enter)[2],id.county(value=loc,type=type.enter)[1]), proj4string = CRS('+proj=longlat +datum=WGS84'))
+  
+  # get mukey info for point
+  mu.info <- SDA_make_spatial_query(p)
+  
+  # query SDA for mukey and soil series info
+  query <- paste("SELECT *
+                 FROM component
+                 WHERE mukey IN (",paste(unique(mu.info$mukey),collapse=","),")")
+  SDA.sub <- SDA_query(query)
+  
+  # use soil series info to pull RaCA data
+  results <- list()
+  
+  for (i in 1:length(SDA.sub$compname)){
+    results[[i]] <- try(fetchRaCA(series=SDA.sub$compname[i]),silent=T)
+  }
+  return(results[[1]]$stock)
+}
