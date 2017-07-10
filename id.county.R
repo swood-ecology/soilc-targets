b183308984d506acce582ca86f8001312b0382b1
Stephen Wood <stephen.wood@CT201-SWOOD.wireless.yale.internal>
2017-07-10 09:48:08 -0400

    Minor formatting changes

--- id.county.R
+++ id.county.R
@@ -1,9 +1,9 @@
 # read in multiple sources of georeference data and return lat/long coordinates
 
 id.county <- function(value,type){
+  # require(ggmap)
   if(type == 'zip'){
     # using ggmap interface with google API
-    require(ggmap)
     geo_reply = geocode(value, output='all', messaging=TRUE, override_limit=TRUE)
     #     county <- geo_reply$results[[1]]$address_components[[5]]$long_name
     return(c(geo_reply$results[[1]]$geometry$location$lat, geo_reply$results[[1]]$geometry$location$lng))
@@ -20,7 +20,6 @@ id.county <- function(value,type){
   else if(type == 'address'){
     # inspiration for this approach from: http://www.shanelynn.ie/massive-geocoding-with-r-and-google-maps/
     # could also do directly from census: https://geocoding.geo.census.gov/geocoder/Geocoding_Services_API.html#_Toc379292356
-    require(ggmap)
     geo_reply = geocode(value, output='all', messaging=TRUE, override_limit=TRUE)
     #     county <- geo_reply$results[[1]]$address_components[[5]]$long_name
     return(c(geo_reply$results[[1]]$geometry$location$lat, geo_reply$results[[1]]$geometry$location$lng))
