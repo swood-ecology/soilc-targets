962a45c8b88159dc13c9caee0ce8df7e42a0d58e
Stephen Wood <stephen.wood@CT201-SWOOD.wireless.yale.internal>
2017-07-10 09:45:47 -0400

    Script only focuses on KSSL; returns 0-100 score

--- SSURGO_scrub.R
+++ SSURGO_scrub.R
@@ -9,8 +9,8 @@
 # load packages
 
 som.score <- function(loc,type.enter,som=1){
-  require(soilDB)         # For querying SSURGO and RaCA
-  require(sp)             # For defining spatial points
+  # require(soilDB)         # For querying SSURGO and RaCA
+  # require(sp)             # For defining spatial points
 
   # determine farm location
   p <- SpatialPoints(cbind(id.county(value=loc,type=type.enter)[2],id.county(value=loc,type=type.enter)[1]), proj4string = CRS('+proj=longlat +datum=WGS84'))
@@ -24,54 +24,67 @@ som.score <- function(loc,type.enter,som=1){
                  WHERE mukey IN (",paste(unique(mu.info$mukey),collapse=","),")")
   SDA.sub <- SDA_query(query)
   
+  ## print soil types
+  # print("Your soil is represented by the following series:")
+  # for(i in 1:length(SDA.sub$compname)){
+  #   print(paste0(SDA.sub$compname[i]," (",SDA.sub$comppct_r[i],"%)"))
+  # }
+  
   # drop mukey components that aren't series
   SDA.sub <- SDA.sub[which(SDA.sub$compkind=='Series'),]
   
   # use soil series info to pull RaCA and KSSL data
-  raca.results <- vector("list", length(SDA.sub$compname))
+  # raca.results <- vector("list", length(SDA.sub$compname))
   kssl.results <- vector("list", length(SDA.sub$compname))
   
-  for (i in 1:length(SDA.sub$compname)){
-    if(try(fetchRaCA(series=SDA.sub$compname[i]),silent=T)[1]!="Error : query returned no data\n"){
-      raca.results[[i]] <- try(fetchRaCA(series=SDA.sub$compname[i]),silent=T)
-      
-      # extract soc for RaCA
-      # only include samples < 40 cm depth; calculate mean of all samples; attach series name
-      raca.soc.means <- data.frame(matrix(NA, nrow=length(raca.results), ncol=2))
-      names(raca.soc.means) <- c('SOC','Series')
-      for (i in 1:length(raca.results)){
-        if(!is.null(raca.results[[i]])){
-          raca.soc.means[i,1] <- mean(raca.results[[i]]$sample[which(raca.results[[i]]$sample$sample_bottom < 25),]$soc,na.rm=T)
-          raca.soc.means[i,2] <- raca.results[[i]]$pedons$taxonname[1]
-        }
-      }
-    }
-    else{
-      print(paste0("No data for the ", SDA.sub$compname[i], " series exists in RaCA"))
-    }  
-  }
+  # raca.soc.means <- data.frame(matrix(NA, nrow=length(raca.results), ncol=2))
+  # names(raca.soc.means) <- c('SOC','Series')
+  # 
+  # for (i in 1:length(SDA.sub$compname)){
+  #   temp = try(fetchRaCA(series=SDA.sub$compname[i]),silent=T)
+  #   if(temp[1]!="Error : query returned no data\n"){
+  #     raca.results[[i]] <- temp
+  #     
+  #     # extract soc for RaCA
+  #     # only include samples < 40 cm depth; calculate mean of all samples; attach series name
+  #     for (i in 1:length(raca.results)){
+  #       if(!is.null(raca.results[[i]])){
+  #         raca.soc.means[i,1] <- mean(raca.results[[i]]$sample[which(raca.results[[i]]$sample$sample_bottom < 25),]$soc,na.rm=T)
+  #         raca.soc.means[i,2] <- raca.results[[i]]$pedons$taxonname[1]
+  #       }
+  #     }
+  #   }
+  # }
+  # remove(temp)
   
   for(i in 1:length(SDA.sub$compname)){
-    kssl.results[[i]] <- try(fetchKSSL(series=SDA.sub$compname[i]),silent=T)  
+    temp = try(fetchKSSL(series=SDA.sub$compname[i]),silent=T)
+    if(!is.null(temp)){
+      kssl.results[[i]] <- temp
+    }
   }
+  remove(temp)
   
   # extract soc for KSSL
   kssl.soc.means <- data.frame(matrix(NA, nrow=length(kssl.results), ncol=2))
   names(kssl.soc.means) <- c('SOM','Series')
   for (i in 1:length(kssl.results)){
-    kssl.soc.means[i,1] <- mean(kssl.results[[i]][which(kssl.results[[i]]$hzn_bot < 25),]$estimated_om,na.rm=T)
-    kssl.soc.means[i,2] <- kssl.results[[i]]$taxonname[1]
+    if(!is.null(kssl.results[[i]])){
+      kssl.soc.means[i,1] <- mean(kssl.results[[i]][which(kssl.results[[i]]$hzn_bot < 25),]$estimated_om,na.rm=T)
+      kssl.soc.means[i,2] <- kssl.results[[i]]$taxonname[1]
+    }
   }
   
-  # convert RaCA soc to % som
-  raca.soc.means$SOM <- raca.soc.means$SOC * 1.62
-  
-  # compare RaCA and KSSL estimate
+  # # convert RaCA soc to % som
+  # raca.soc.means$SOM <- raca.soc.means$SOC * 1.62
   
   # take weighted averages based on mukey representation and print
   weight <- SDA.sub$comppct_r
-  print(paste0("RaCA SOM%: ", weighted.mean(x=raca.soc.means$SOM,w=weight,na.rm=T)))
-  print(paste0("KSSL SOM%: ", weighted.mean(x=kssl.soc.means$SOM,w=weight,na.rm=T)))
+  # print(paste0("RaCA SOM%: ", weighted.mean(x=raca.soc.means$SOM,w=weight,na.rm=T)))
+  # print(paste0("KSSL SOM%: ", weighted.mean(x=kssl.soc.means$SOM,w=weight,na.rm=T)))
   
-  #return(input/target*100)
+  # create data frame of all information
+  final_data <- data.frame(kssl.soc.means,SDA.sub$comppct_r)
+  
+  return((som/weighted.mean(x=kssl.soc.means$SOM,w=weight,na.rm=T))*100)
 }
